0b817fb7e9b6d2942928f9510ecb1e3f1337634e
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-08 15:35:44 -0700

    A

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Des : print all chars in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char x;
+	int x;
+
+	for (c = 0; c <= 9; ++c)
+		putchar(c);
+	for (x = 'a'; x <= 'f'; ++x)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
