ac4d985434dfd921ed9503cbed0cbad6e9474670
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-08 14:25:58 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Des : print all chars in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; ++C)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
