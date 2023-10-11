0548f31b4a20357277f6b8a30efed49660486fb0
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2023-10-11 18:52:13 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,4 @@
 
 void jack_bauer(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 9; ++a)
-{
-for (b = 0; b <= 9; ++b)
-{
-for (c = 0; c <= 9; ++c)
-{
-for (d = 0; d <= 9; ++d)
-{
-printf("%d%d:%d%d\n", a, b, c, d);
-}
-}
-}
-}
 }
