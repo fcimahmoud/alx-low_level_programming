55551d02427950c68d8d4ca1fcc5149081871d6e
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2023-10-11 18:31:48 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * jack_bauer - function to print the last digit of n
+ * jack_bauer - function to print every minute of the day of Jack Bauer
  *
- * @n: checks input of function
- *
- * Return: returns the value of the last digit
+ * Return: prints every minute of the day of Jack Bauer
 */
 
 void jack_bauer(void)
 {
-  int a, b, c, d;
-
-  for (a = 0; a <= 9; ++a)
-    {
-      for (b = 0; b <= 9; ++b)
-        {
-          for (c = 0; c <= 9; ++c)
-            {
-              for (d = 0; d <= 9; ++d)
-                {
-                  printf("%d%d : %d%d \n", a, b, c, d);
-                }
-            }
-        }
-    }
+int a, b, c, d;
+for (a = 0; a <= 9; ++a)
+{
+for (b = 0; b <= 9; ++b)
+{
+for (c = 0; c <= 9; ++c)
+{
+for (d = 0; d <= 9; ++d)
+{
+printf("%d%d:%d%d\n", a, b, c, d);
+}
+}
+}
+}
 }
