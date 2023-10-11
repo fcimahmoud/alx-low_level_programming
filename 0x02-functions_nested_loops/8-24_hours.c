d9a5c72a505bc15248f6ba10c00d11665e16a39c
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-11 11:00:05 -0700

    AA

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to print every minute of the day of Jack Bauer
+ *
+ * Return: prints every minute of the day of Jack Bauer
+*/
+
+void jack_bauer(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; ++a)
+	{
+		for (b = 0; b <= 5; ++b)
+		{
+			for (c = 0; c <= 9; ++C)
+			{
+				printf("0%d:%d%d\n", a, b, c);
+			}
+		}
+	}
+	for (a = 0; a <= 2; ++a)
+	{
+		for (b = 0; b <= 5; ++b)
+		{
+			for (c = 0; c <= 9; ++c)
+			{
+				printf("1%d:%d%d\n", a, b, c);
+			}
+		}
+	}
+}
