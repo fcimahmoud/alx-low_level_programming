47073f665fe9df762da8793286ec6767c9cc3871
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-11 13:11:20 -0700

    A

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to return sum of tow integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: the sum of 2 integers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
