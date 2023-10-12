023406b6b148dd65f62b1c966c73724ef38eb245
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-12 15:42:38 -0700

    W

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers from 0 to 9
+ *
+ * Return: print numbers from 0 to 9
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		_putchar(i);
+	}
+}
