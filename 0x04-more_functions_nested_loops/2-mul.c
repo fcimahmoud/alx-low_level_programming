6a03a6aeab741478ddbe7d4c8e5ce4bcd75bac59
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-12 15:26:27 -0700

    SWA

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies tow integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: multiple of tow numbers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
