f4b56b58647b2a7eba00c70446354ca9fe64adae
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-12 16:21:23 -0700

    sas

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - print diagonal line
+ *
+ * @n: number of times the \ char should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int p, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (p = 1; p <= n; ++p)
+		{
+			for (s = 1; s <= p; ++s)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
