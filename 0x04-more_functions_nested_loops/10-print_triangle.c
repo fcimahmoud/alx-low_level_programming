3fb9f6cea8df4bae6f26e883246e2b3f5f44523b
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-13 02:09:07 -0700

    W

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a triangle
+ *
+ * @size: input for function
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+	for (i = 0; i < size; ++i)
+	{
+		for (j = size - i - 1; j > 0; --j)
+			_putchar(' ');
+		for (j = 0; j <= i; ++j)
+			_putchar('#');
+	}
+	}
+	_putchar('\n');
+}
