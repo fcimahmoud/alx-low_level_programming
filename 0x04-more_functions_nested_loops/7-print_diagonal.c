fb318e109091b0861abe66fc3b7cb87ebe5b45c7
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-13 01:31:08 -0700

    W

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,45 +1,32 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagonal - Write a function that draws a diagonal
- * line on the terminal.
- * from 0 to 14, followed by a new line.
- * @numberOfTimesTheCharacter: is the number of times the
- * character \ should be printed
- * You can only use _putchar three times in your
+ * print_diagonal - Write a a function that draws a diagonal line
+ *
+ * @n: Number of times to print
+ *
+ * You can only use _putchar in your code
  */
 
-void print_diagonal(int numberOfTimesTheCharacter)
+void print_diagonal(int n)
 {
-	int length, space;
+	int i, j;
 
-	if (numberOfTimesTheCharacter > 0)
+	if (n <= 0)
 	{
-		/*Outer loop controls the number of rows in the diagonal pattern.*/
-		for (length = 0; length < numberOfTimesTheCharacter; length++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; ++i)
 		{
-			/* Inner loop is used to print spaces before */
-			/* the backslash to create the diagonal effect.  */
-			for (space = 0; space < length; space++)
+			for (j = 0; j < i; ++j)
 			{
 				_putchar(' ');
 			}
-
-			/* Print the backslash character. */
 			_putchar('\\');
-
-			/* Check if it's the last row, and if not, move to the next line. */
-			if (length == (numberOfTimesTheCharacter - 1))
-			{
-				continue;
-			}
-			for (s = 1; s <= p; ++s)
-				_putchar(' '); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
-
-	/* After the diagonal pattern is printed, print a */
-	/* newline to separate it from other output. */
-	_putchar('\n');
 }
