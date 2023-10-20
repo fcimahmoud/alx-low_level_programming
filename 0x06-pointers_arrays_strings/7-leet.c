48f13c87d006cb37295a367bffda5c1500143110
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2023-10-20 17:56:11 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,24 +9,24 @@
 
 char *leet(char *c)
 {
-  char *cp = c;
-  char key[] = {'A', 'E', 'O', 'T', 'L'};
-  int value[] = {4, 3, 0, 7, 1};
-  unsigned int i;
+char *cp = c;
+char key[] = {'A', 'E', 'O', 'T', 'L'};
+int value[] = {4, 3, 0, 7, 1};
+unsigned int i;
 
-  while (*c)
-    {
-      for (i = 0; i < sizeof(key) / sizeof(char); i++)
-        {
-          if (*c == key[i] || *c == key[i] + 32)
-          {
-            *c = 48 + value[i];
-          }
-        }
-      c++;
-    }
+while (*c)
+{
+for (i = 0; i < sizeof(key) / sizeof(char); i++)
+{
+if (*c == key[i] || *c == key[i] + 32)
+{
+*c = 48 + value[i];
+}
+}
+c++;
+}
 
-  return (cp);
+return (cp);
 }
 
 
