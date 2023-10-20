ddb0e441bceea5465e57f126eb90e23b5c37fa8a
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2023-10-20 16:51:58 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,15 @@
 
 char *string_toupper(char *)
 {
-      int i;
+int i;
 
-      for (i = 0; str[i] != '\0'; i++)
-      {
-            if (str[i] >= 97 && str[i] <= 122)
-            {
-                  str[i] = str[i] - 32;
-            }
-      }
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+}
 
-      return (str);
+return (str);
 }
