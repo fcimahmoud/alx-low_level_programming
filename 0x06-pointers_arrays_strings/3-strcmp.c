2d778109c4040fbde1f6b2e8366439d4f9667847
Blyatsho <98816711+fcimahmoud@users.noreply.github.com>
2023-10-20 10:55:05 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,29 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - Write a function that compares two strings.
+ * Prototype: int _strcmp(char *s1, char *s2);
+ * Your function should work exactly like strcmp
+ */
+
+int _strcmp(char *firstString, char *secondString)
+{
+
+	while (*firstString != '\0' && *secondString != '\0')
+	{
+		if (*firstString < *secondString)
+		{
+			output = *firstString - *secondString;
+			return (output);
+		}
+		else if (*firstString > *secondString)
+		{
+			output = *firstString - *secondString;
+			return (output);
+		}
+		firstString++;
+		secondString++;
+	}
+	return (0);
+}
