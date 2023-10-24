57c2806f894de18f4561c85931eb7b440f775fd7
fcimahmoud <ma5740@fayoum.edu.eg>
2023-10-24 01:05:03 +0100

    a

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_diagsums - function that prints the chessboard.
  * @a: 2D array off int types
@@ -11,12 +12,12 @@ void print_diagsums(int *a, int size)
 {
 	int i, s1 = 0, s2 = 0;
 
-	for (i = 0 ; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		s1 += a[i];
 		s2 += a[size - i -  1];
 		a += size;
 	}
 	printf("%d, ", s1);
-	printf("%d\n, s2");
+	printf("%d\n", s2);
 }
